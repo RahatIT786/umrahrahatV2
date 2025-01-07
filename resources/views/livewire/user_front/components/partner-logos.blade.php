f5a024c03f3f932b9ebcdd696f83b329db02a148
RahatIT786 <rahatit486@gmail.com>
2025-01-07 12:25:37 +0530

    chore: Home page dummy text removed

--- resources/views/livewire/user_front/components/partner-logos.blade.php
+++ resources/views/livewire/user_front/components/partner-logos.blade.php
@@ -1,78 +1,21 @@
-{{-- 93c5fcf --}}
-<div  class="elementor-element elementor-element-{{$dataId}} e-flex e-con-boxed e-con e-parent partner-logo" data-id="{{$dataId}}" data-element_type="container" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}">
-        <div class="e-con-inner">
-            <div class="elementor-element elementor-element-aab9a6a e-flex e-con-boxed e-con e-child" data-id="aab9a6a" data-element_type="container">
-                <div class="e-con-inner">
-                    <div class="elementor-element elementor-element-6a488aa elementor-widget elementor-widget-image" data-id="6a488aa" data-element_type="widget" data-widget_type="image.default">
-                        <div class="elementor-widget-container">
-                            <img loading="lazy" decoding="async" width="642" height="183" src="{{ asset('asserts/user/img/img1.png') }}" class="attachment-large size-large wp-image-229" alt="" sizes="(max-width: 642px) 100vw, 642px" /> 
-                        </div>
+<div class="elementor-element elementor-element-6471640 elementor-widget elementor-widget-image-carousel" data-id="6471640" data-element_type="widget" data-settings="{&quot;slides_to_show&quot;:&quot;4&quot;,&quot;navigation&quot;:&quot;none&quot;,&quot;image_spacing_custom&quot;:{&quot;unit&quot;:&quot;px&quot;,&quot;size&quot;:100,&quot;sizes&quot;:[]},&quot;slides_to_show_tablet&quot;:&quot;4&quot;,&quot;slides_to_show_mobile&quot;:&quot;3&quot;,&quot;image_spacing_custom_tablet&quot;:{&quot;unit&quot;:&quot;px&quot;,&quot;size&quot;:30,&quot;sizes&quot;:[]},&quot;image_spacing_custom_mobile&quot;:{&quot;unit&quot;:&quot;px&quot;,&quot;size&quot;:15,&quot;sizes&quot;:[]},&quot;autoplay&quot;:&quot;yes&quot;,&quot;pause_on_hover&quot;:&quot;yes&quot;,&quot;pause_on_interaction&quot;:&quot;yes&quot;,&quot;autoplay_speed&quot;:5000,&quot;infinite&quot;:&quot;yes&quot;,&quot;speed&quot;:500}"
+     data-widget_type="image-carousel.default">
+       <div class="elementor-widget-container">
+           <div class="elementor-image-carousel-wrapper swiper" role="region" aria-roledescription="carousel" aria-label="Image Carousel" dir="ltr">
+                <div class="elementor-image-carousel swiper-wrapper" aria-live="off">
+                    <div class="swiper-slide" role="group" aria-roledescription="slide" aria-label="1 of 4">
+                        <figure class="swiper-slide-inner"><img decoding="async" class="swiper-slide-image" src="{{ asset('asserts/user/img/img1.png') }}" style="height: 6rem;"  alt="Image"></figure>
+                    </div>
+                    <div class="swiper-slide" role="group" aria-roledescription="slide" aria-label="2 of 4">
+                        <figure class="swiper-slide-inner"><img decoding="async" class="swiper-slide-image" src="{{ asset('asserts/user/img/img2.png') }}" style="height: 4rem;" alt="Logo-2" /></figure>
+                    </div>
+                    <div class="swiper-slide" role="group" aria-roledescription="slide" aria-label="3 of 4">
+                     <figure class="swiper-slide-inner"><img decoding="async" class="swiper-slide-image" src="{{ asset('asserts/user/img/img3.png') }}" style="height: 4rem;" alt="Logo-3" /></figure>
+                    </div>
+                    <div class="swiper-slide" role="group" aria-roledescription="slide" aria-label="4 of 4">
+                     <figure class="swiper-slide-inner"><img decoding="async" class="swiper-slide-image" src="{{ asset('asserts/user/img/img4.png') }}" style="height: 6rem;" alt="Logo-4" /></figure>
                     </div>
                 </div>
-            </div>
-            <div class="elementor-element elementor-element-934d00a e-flex e-con-boxed e-con e-child" data-id="934d00a" data-element_type="container">
-                <div class="e-con-inner">
-                    <div class="elementor-element elementor-element-15cb7fd elementor-widget elementor-widget-image" data-id="15cb7fd" data-element_type="widget" data-widget_type="image.default">
-                        <div class="elementor-widget-container">
-                            <img loading="lazy" decoding="async" width="642" height="183" src="{{ asset('asserts/user/img/img2.png') }}" class="attachment-large size-large wp-image-229" alt="" sizes="(max-width: 642px) 100vw, 642px" /> 
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="elementor-element elementor-element-ae22185 e-flex e-con-boxed e-con e-child" data-id="ae22185" data-element_type="container">
-                <div class="e-con-inner">
-                    <div class="elementor-element elementor-element-66b181b elementor-widget elementor-widget-image" data-id="66b181b" data-element_type="widget" data-widget_type="image.default">
-                        <div class="elementor-widget-container">
-                           <img loading="lazy" decoding="async" width="642" height="183" src="{{ asset('asserts/user/img/img3.png') }}" class="attachment-large size-large wp-image-229" alt="" sizes="(max-width: 642px) 100vw, 642px" /> 
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="elementor-element elementor-element-b9c53ce e-flex e-con-boxed e-con e-child" data-id="b9c53ce" data-element_type="container">
-                <div class="e-con-inner">
-                    <div class="elementor-element elementor-element-adfc3ce elementor-widget elementor-widget-image" data-id="adfc3ce" data-element_type="widget" data-widget_type="image.default">
-                        <div class="elementor-widget-container">
-                           <img loading="lazy" decoding="async" width="642" height="183" src="{{ asset('asserts/user/img/img4.png') }}" class="attachment-large size-large wp-image-229" alt="" sizes="(max-width: 642px) 100vw, 642px" /> 
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="elementor-element elementor-element-b9c53ce e-flex e-con-boxed e-con e-child" data-id="b9c53ce" data-element_type="container">
-                <div class="e-con-inner">
-                    <div class="elementor-element elementor-element-adfc3ce elementor-widget elementor-widget-image" data-id="adfc3ce" data-element_type="widget" data-widget_type="image.default">
-                        <div class="elementor-widget-container">
-                           <img loading="lazy" decoding="async" width="642" height="183" src="{{ asset('asserts/user/img/img5.png') }}" class="attachment-large size-large wp-image-229" alt="" sizes="(max-width: 642px) 100vw, 642px" /> 
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- <div class="elementor-element elementor-element-b9c53ce e-flex e-con-boxed e-con e-child" data-id="b9c53ce" data-element_type="container">
-                <div class="e-con-inner">
-                    <div class="elementor-element elementor-element-adfc3ce elementor-widget elementor-widget-image" data-id="adfc3ce" data-element_type="widget" data-widget_type="image.default">
-                        <div class="elementor-widget-container">
-                           <img loading="lazy" decoding="async" width="642" height="183" src="{{ asset('asserts/user/img/img6.png') }}" class="attachment-large size-large wp-image-229" alt="" sizes="(max-width: 642px) 100vw, 642px" /> 
-                        </div>
-                    </div>
-                </div>
-            </div> -->
-            <div class="elementor-element elementor-element-b9c53ce e-flex e-con-boxed e-con e-child" data-id="b9c53ce" data-element_type="container">
-                <div class="e-con-inner">
-                    <div class="elementor-element elementor-element-adfc3ce elementor-widget elementor-widget-image" data-id="adfc3ce" data-element_type="widget" data-widget_type="image.default">
-                        <div class="elementor-widget-container">
-                           <img loading="lazy" decoding="async" width="642" height="183" src="{{ asset('asserts/user/img/img7.png') }}" class="attachment-large size-large wp-image-229" alt="" sizes="(max-width: 642px) 100vw, 642px" /> 
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="elementor-element elementor-element-b9c53ce e-flex e-con-boxed e-con e-child" data-id="b9c53ce" data-element_type="container">
-                <div class="e-con-inner">
-                    <div class="elementor-element elementor-element-adfc3ce elementor-widget elementor-widget-image" data-id="adfc3ce" data-element_type="widget" data-widget_type="image.default">
-                        <div class="elementor-widget-container">
-                           <img loading="lazy" decoding="async" width="642" height="183" src="{{ asset('asserts/user/img/img8.png') }}" class="attachment-large size-large wp-image-229" alt="" sizes="(max-width: 642px) 100vw, 642px" /> 
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> 
-
+             </div>
+         </div>
+</div>
