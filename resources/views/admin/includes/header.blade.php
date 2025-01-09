8b4d21989e5e551c99d7bee94873be8331683599
RahatIT786 <rahatit486@gmail.com>
2025-01-09 19:32:41 +0530

    fix: Path fix in word change admin files

--- resources/views/admin/includes/header.blade.php
+++ resources/views/admin/includes/header.blade.php
@@ -7,15 +7,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="assets/images/favicon-32x32.png" type="image/png" />
   <!--plugins-->
-  <link href="public/assets/plugins/vectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet"/>
-  <link href="public/assets/plugins/simplebar/css/simplebar.css" rel="stylesheet" />
-  <link href="public/assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" />
-  <link href="public/assets/plugins/metismenu/css/metisMenu.min.css" rel="stylesheet" />
+  <link href="public/asserts/plugins/vectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet"/>
+  <link href="public/asserts/plugins/simplebar/css/simplebar.css" rel="stylesheet" />
+  <link href="public/asserts/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" />
+  <link href="public/asserts/plugins/metismenu/css/metisMenu.min.css" rel="stylesheet" />
   <!-- Bootstrap CSS -->
-  <link href="public/assets/css/bootstrap.min.css" rel="stylesheet" />
-  <link href="public/assets/css/bootstrap-extended.css" rel="stylesheet" />
-  <link href="public/assets/css/style.css" rel="stylesheet" />
-  <link href="public/assets/css/icons.css" rel="stylesheet">
+  <link href="public/asserts/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="public/asserts/css/bootstrap-extended.css" rel="stylesheet" />
+  <link href="public/asserts/css/style.css" rel="stylesheet" />
+  <link href="public/asserts/css/icons.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
   
@@ -24,10 +24,10 @@
 	<link href="assets/css/pace.min.css" rel="stylesheet" />
 
   <!--Theme Styles-->
-  <link href="public/assets/css/dark-theme.css" rel="stylesheet" />
-  <link href="public/ssets/css/light-theme.css" rel="stylesheet" />
-  <link href="public/assets/css/semi-dark.css" rel="stylesheet" />
-  <link href="public/assets/css/header-colors.css" rel="stylesheet" />
+  <link href="public/asserts/css/dark-theme.css" rel="stylesheet" />
+  <link href="public/asserts/css/light-theme.css" rel="stylesheet" />
+  <link href="public/asserts/css/semi-dark.css" rel="stylesheet" />
+  <link href="public/asserts/css/header-colors.css" rel="stylesheet" />
 
   <title>Snacked - Bootstrap 5 Admin Template</title>
 </head>
