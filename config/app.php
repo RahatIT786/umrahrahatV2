c9fd0ee21ff17fb179fca6b10b3624f50c8d763a
abu <rahatit486@gmail.com>
2025-01-10 17:57:02 +0530

    path big problem fixed

--- config/app.php
+++ config/app.php
@@ -122,13 +122,5 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
-
-
-    'providers' => [
-    // Other service providers...
-
-    App\Providers\CustomPublicPathServiceProvider::class,
-],
-
-
+    
 ];
