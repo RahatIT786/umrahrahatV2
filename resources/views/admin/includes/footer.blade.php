dc0e80dbaab582a17c4c791d37c3de4e4804ce47
RahatIT786 <rahatit486@gmail.com>
2025-01-11 13:53:04 +0530

    fix: admin panal error

---
+++ resources/views/admin/includes/footer.blade.php
@@ -0,0 +1,23 @@
+<footer class="footer card mb-0 rounded-0 justify-content-center align-items-center">
+  <!-- Bootstrap bundle JS -->
+  <script src="public/asserts/js/bootstrap.bundle.min.js"></script>
+  <!--plugins-->
+  <script src="public/asserts/js/jquery.min.js"></script>
+  <script src="public/asserts/plugins/simplebar/js/simplebar.min.js"></script>
+  <script src="public/asserts/plugins/metismenu/js/metisMenu.min.js"></script>
+  <script src="public/asserts/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
+  <script src="public/asserts/js/pace.min.js"></script>
+  <script src="public/asserts/plugins/chartjs/js/Chart.min.js"></script>
+  <script src="public/asserts/plugins/chartjs/js/Chart.extension.js"></script>
+  <script src="public/asserts/plugins/apexcharts-bundle/js/apexcharts.min.js"></script>
+   <!-- Vector map JavaScript -->
+   <script src="public/asserts/plugins/vectormap/jquery-jvectormap-2.0.2.min.js"></script>
+   <script src="public/asserts/plugins/vectormap/jquery-jvectormap-world-mill-en.js"></script>
+  <!--app-->
+  <script src="public/asserts/js/app.js"></script>
+  <script src="public/asserts/js/index.js"></script>
+  <script>
+    new PerfectScrollbar(".review-list")
+    new PerfectScrollbar(".chat-talk")
+ </script>
+</footer>
