f162f517e912101f33e81f65838bf4276b071422
Rahat IT Solutions <rahatit486@gmail.com>
2025-01-18 13:49:40 +0530

    Update optimize.php

--- public/optimize.php
+++ public/optimize.php
@@ -1,6 +1,10 @@
 
 <?php
 exec('php artisan optimize');
+
+exec('php artisan route:clear');
+exec('php artisan view:clear');
+exec('php artisan optimize');
 exec('php artisan config:cache');
 exec('php artisan config:clear');
 echo "Application optimized!";
