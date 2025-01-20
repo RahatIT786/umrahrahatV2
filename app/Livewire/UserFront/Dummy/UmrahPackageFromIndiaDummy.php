9979164032dfec415bc4554037bf8ee6579c80b7
RahatIT786 <rahatit486@gmail.com>
2025-01-20 12:38:32 +0530

    umrah Dummy

---
+++ app/Livewire/UserFront/Dummy/UmrahPackageFromIndiaDummy.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\UserFront\Dummy;
+
+use Livewire\Component;
+
+class UmrahPackageFromIndiaDummy extends Component
+{
+    public function render()
+    {
+        return view('livewire.user_front.dummy.umrah-package-from-india-dummy');
+    }
+}
