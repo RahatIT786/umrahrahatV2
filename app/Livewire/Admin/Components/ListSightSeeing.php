4fe4535472686c70778b545959b0f2926f3dad7c
RahatIT786 <rahatit486@gmail.com>
2025-01-29 20:01:10 +0530

    sight seeing

--- app/Livewire/Admin/Components/ListSightSeeing.php
+++ app/Livewire/Admin/Components/ListSightSeeing.php
@@ -4,12 +4,44 @@ namespace App\Livewire\Admin\Components;
 
 use Livewire\Component;
 use Livewire\Attributes\Layout;
+use Livewire\WithPagination; 
+use App\Models\sightController;
 
 class ListSightSeeing extends Component
 {
+    use WithPagination;  
+    public $search = null;
+    public $sightId;  
+    public $showModal = false;
+    public function confirmDelete($id)
+    {
+        $this->sightId  = $id;
+        $this->showModal = true;
+    }
+
+    public function closeModal()
+    {
+        $this->showModal = false;
+    }
+    public function deleteData()
+    {
+        if ($this->sightId) {
+            $SightDetails = sightController::findOrFail($this->sightId);
+            $SightDetails->update(['delete_status' => 2]);
+            session()->flash('message', 'Sight details successfully deleted.');
+            $this->showModal = false;
+        }
+    }
     #[Layout('admin.Layouts.app')]
     public function render()
     {
-        return view('livewire.admin.components.list-sight-seeing');
+        $SightDetails = sightController::where('delete_status', 1)
+        ->where(function($query){
+            $query->where('sightName', 'like', '%'.$this->search.'%')
+                  ->orWhere('sightPrice', 'like', '%'.$this->search.'%')
+                  ->orWhere('sightCity', 'like', '%'.$this->search.'%');
+        })
+        ->paginate(10);
+        return view('livewire.admin.components.list-sight-seeing',['sightDetail' => $SightDetails]);
     }
 }
