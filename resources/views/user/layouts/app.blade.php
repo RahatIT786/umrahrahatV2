a9a242541443929eae812ac766c892246ad52028
RahatIT786 <rahatit486@gmail.com>
2025-01-31 16:20:50 +0530

    navbar color

--- resources/views/user/layouts/app.blade.php
+++ resources/views/user/layouts/app.blade.php
@@ -58,9 +58,6 @@
       height: 60px;
       /* box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); */
       box-shadow: rgb(113, 123, 134) 0px 20px 30px -10px;
-      /* box-shadow: rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px; */
-      /* box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px; */
-      /* border: 2px solid black; */
     }
 
     .nav-item-package {
