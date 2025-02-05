729646d92906483bb2966b8c7637cd99af2a52e9
RahatIT786 <rahatit486@gmail.com>
2025-02-05 21:17:04 +0530

    img error fixed

--- resources/views/livewire/user_front/umrahv2/catring.blade.php
+++ resources/views/livewire/user_front/umrahv2/catring.blade.php
@@ -45,9 +45,7 @@
         <div class="container-xxl py-5">
             <div class="container">
                 <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                    <!-- <h6 class="section-title text-center text-primary text-uppercase">cghf</h6> -->
-                    <!-- <h1 class="mb-5">Our Catering Service<span class="text-primary text-uppercase"></span></h1> -->
-                     <img src="{{}}" alt="">
+                    
                 </div>
                 <div class="row g-4 align-items-center">
                     @foreach ($foodDetails as $food)
