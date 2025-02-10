268e5c8603e3c8cecf9492bac1bef67a90290401
RahatIT786 <rahatit486@gmail.com>
2025-02-10 21:03:37 +0530

    add user

--- resources/views/agent/Layouts/app.blade.php
+++ resources/views/agent/Layouts/app.blade.php
@@ -12,17 +12,17 @@
         <link rel="shortcut icon" href="assets/images/favicon.ico">
 
         <!-- plugin css -->
-        <link href="{{asset('assets/agent/assets/libs/jsvectormap/jsvectormap.min.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{asset('asserts/agent/assets/libs/jsvectormap/jsvectormap.min.css')}}" rel="stylesheet" type="text/css" />
 
         <!-- swiper css -->
         <link rel="stylesheet" href="{{asset('assets/agent/assets/libs/swiper/swiper-bundle.min.css')}}">
 
         <!-- Bootstrap Css -->
-        <link href="{{asset('assets/agent/assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+        <link href="{{asset('asserts/agent/assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />
         <!-- Icons Css -->
-        <link href="{{asset('assets/agent/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{asset('asserts/agent/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
         <!-- App Css-->
-        <link href="{{asset('assets/agent/assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
+        <link href="{{asset('asserts/agent/assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
 
     </head>
 
@@ -46,24 +46,24 @@
     <div class="rightbar-overlay"></div>
 
     <!-- JAVASCRIPT -->
-    <script src="{{asset('assets/agent/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-    <script src="{{asset('assets/agent/assets/libs/metismenujs/metismenujs.min.js')}}"></script>
-    <script src="{{asset('assets/agent/assets/libs/simplebar/simplebar.min.js')}}"></script>
-    <script src="{{asset('assets/agent/assets/libs/feather-icons/feather.min.js')}}"></script>
+    <script src="{{asset('asserts/agent/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('asserts/agent/assets/libs/metismenujs/metismenujs.min.js')}}"></script>
+    <script src="{{asset('asserts/agent/assets/libs/simplebar/simplebar.min.js')}}"></script>
+    <script src="{{asset('asserts/agent/assets/libs/feather-icons/feather.min.js')}}"></script>
 
     <!-- apexcharts -->
-    <script src="{{asset('assets/agent/assets/libs/apexcharts/apexcharts.min.js')}}"></script>
+    <script src="{{asset('asserts/agent/assets/libs/apexcharts/apexcharts.min.js')}}"></script>
 
     <!-- Vector map-->
-    <script src="{{asset('assets/agent/assets/libs/jsvectormap/jsvectormap.min.js')}}"></script>
-    <script src="{{asset('assets/agent/assets/libs/jsvectormap/maps/world-merc.js')}}"></script>
+    <script src="{{asset('asserts/agent/assets/libs/jsvectormap/jsvectormap.min.js')}}"></script>
+    <script src="{{asset('asserts/agent/assets/libs/jsvectormap/maps/world-merc.js')}}"></script>
 
     <!-- swiper js -->
-    <script src="{{asset('assets/agent/assets/libs/swiper/swiper-bundle.min.js')}}"></script>
+    <script src="{{asset('asserts/agent/assets/libs/swiper/swiper-bundle.min.js')}}"></script>
 
-    <script src="{{asset('assets/agent/assets/js/pages/dashboard.init.js')}}"></script>
+    <script src="{{asset('asserts/agent/assets/js/pages/dashboard.init.js')}}"></script>
 
-    <script src="{{asset('assets/agent/assets/js/app.js')}}"></script>
+    <script src="{{asset('asserts/agent/assets/js/app.js')}}"></script>
 
     </body>
 
